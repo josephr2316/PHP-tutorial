63d20996c35163a11fba801a06566894030c5702
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-16 14:53:41 -0400

    Integer casting and verification

--- integer.php
+++ integer.php
@@ -31,8 +31,81 @@ echo $x . '<br/>';
 echo '<br/>';
 var_dump($x);
 
-$x = PHP_INT_MAX + 1; // It becomes a float number
+$x = PHP_INT_MAX + 1; // It becomes a float number, out of bounds of int
 echo $x . '<br/>';
 echo '<br/>';
 var_dump($x);
+
+$x = (int) (PHP_INT_MAX + 1);
+echo $x . '<br/>';
+var_dump($x);
+
+$x = (integer) (PHP_INT_MAX + 1);
+echo $x . '<br/>';
+var_dump($x);
+
+$x = (integer) true;
+echo $x . '<br/>';
+var_dump($x);
+
+$x = (integer) false;
+echo $x . '<br/>';
+var_dump($x);
+
+$x = (int) 5.98; // rounded down
+echo $x . '<br/>';
+var_dump($x);
+
+$x = (int) '5.98'; // rounded down
+echo $x . '<br/>';
+var_dump($x);
+
+$x = (int) '87'; //
+echo $x . '<br/>';
+var_dump($x);
+
+$x = (int) '87awdwada'; // It will print until it found a character
+echo $x . '<br/>';
+var_dump($x);
+
+
+$x = (int) '87awdwada77'; //  It will print until it found a character
+echo $x . '<br/>';
+var_dump($x);
+
+$x = (int) 'awdwada77'; //  It will be converted into 0
+echo $x . '<br/>';
+var_dump($x);
+
+$x = (int) null; //  It will be converted into 0
+echo $x . '<br/>';
+var_dump($x);
+
+$x = (int) 'awdwada77'; //  It will be converted into 0
+echo $x . '<br/>';
+var_dump(is_int($x));
+
+$x = (int) null; //  It will be converted into 0
+echo $x . '<br/>';
+var_dump(is_int($x));
+
+$x =  null; //  It will be converted into 0
+echo $x . '<br/>';
+var_dump(is_int($x));
+
+$x =  200_000; //  To make it more readable
+echo $x . '<br/>';
+var_dump(is_int($x));
+
+$x =  2_000_000_000; //  To make it more readable
+echo $x . '<br/>';
+var_dump(is_int($x));
+
+$x =  '2_000_000_000'; //  But this won't work with string or casting
+echo $x . '<br/>';
+var_dump(is_int($x));
+
+$x =  (int)'2_000_000_000'; //  But this won't work with string or casting
+echo $x . '<br/>';
+var_dump(is_int($x));
 
