1cbd882dd8f37835dce51024e542c317aaccc236
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-16 02:12:58 -0400

    Constansts, and magic constants, also a little bit of knowledge

--- index3.php
+++ index3.php
@@ -14,6 +14,25 @@ echo STATUS_PAID;
 echo defined('STATUS_PAID');
 echo defined('STATUS_VOID'); // Nothing gets printed, it's not defined
 echo STATUS;
+echo defined('STATUS');
+
+if (true){
+//    const FOO = 'bar'; // Won't work
+    define('NEW_Paid', 9);
+}
+
+$paid = 'PAID2';
+
+//define('STATUS_' . $paid, $paid);
+define('STATUS_' . $paid, 88);
+echo STATUS_PAID2;
+echo "\n";
+echo PHP_VERSION;
+echo "\n";
+echo __LINE__; //Their value changes
+echo "\n";
+echo __LINE__; //Their value changes
+
 
 
 
