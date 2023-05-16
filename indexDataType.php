d5a8a02b1b8371dae00222be0bd808c5c7a539f3
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-16 02:36:57 -0400

    Data Type

---
+++ indexDataType.php
@@ -0,0 +1,30 @@
+<!--//  Dynamically Typed can change after it's been defined, happens at run time -->
+<!--//  Static Typed, happens at compile time-->
+
+<?php
+/* Data Types & Type Casting */
+
+#   4   Scalar Types
+        #   bool - true / false
+        $completed = true; # True will print 1 and false will be blank
+        #   int - 1, 2, 3, 0, -5 (no decimal)
+        $score = 75;
+        #   float - 1.5, 0.1, 0.005, -15.8
+        $price = 0.99;
+        #   string - "Joseph", "Hello World"
+        $greeting = "Hello Joseph";
+
+        echo $completed . '<br/>';
+        echo $score . '<br/>';
+        echo $price . '<br/>';
+        echo $greeting . '<br/>';
+
+#  4    Compound Types
+        #   array
+        #   object
+        #   callable
+        #   iterable
+
+#  2    Special Types
+        #   resource
+        #   null
