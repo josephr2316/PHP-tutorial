554820cab8cefd1a135dfa613464df84662a75ef
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-16 01:30:20 -0400

    Implementing php and html

--- index1.php
+++ index1.php
@@ -2,11 +2,11 @@
 //The closing tag should be use if the code it aint' 100% percent php
 
 echo 'Hello World';
-echo print'Hello World';
+echo print'Hello World'; //Print return a value of 1, this print 1 hello world
 //print echo 'Hello World' won't work
-echo ("Hello World");
+echo ("Hello World"); //It's faster
 print("Hello World");
-echo "Hello", " ", "World"; //Concate with echo, I can't do this with print, echo is faster
+echo "Hello", " ", "World"; //Concatenate with echo, I can't do this with print, echo is faster
 echo "Joe's Invoice";
 echo 'Joe\'s Invoice';
 $name = 'Joseph';
@@ -32,7 +32,7 @@ echo "Hello ". $firstName;
 
 
 
-//$this can't be use;
+//$this can't be used;
 //Variable can't have special character, only letters from a-z (lower and uppercase) and it can begin with undercore
 
 //We don't need the semi-colong in the last line if with close the tag
--- index2.php
+++ index2.php
@@ -1,11 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
+<!--Don't mix directly html in your php-->
     <body>
-        <h1><?php echo 'Hello World' ?>
+
+        <h1><?php
+            echo 'Hello World' ?>
             <?= 'Hello World' ?>
 
         </h1>
         <h1>My First Heading</h1>-
         <p>My first paragraph.</p>
+
+        <?php
+            // Comment 1
+            # Comment 2
+        /*
+            Multi Line Comment
+         */
+
+        /**
+         * Documentation for your source code
+         */
+            $x = 10;
+            $y = 5;
+            // Don't mix directly html in your php, presentation logic and business logic
+             echo '<p>' . $x . ',' . $y . '</p>';
+        ?>
     </body>
 </html>
